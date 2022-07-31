61f99b6a48833b9464c9847a4cad1cdcdfaab773
PrintHolamundo <jdohca@gmail.com>
2022-07-31 16:18:46 -0500

    Metodos asincronos

--- src/bases/03-classes.ts
+++ src/bases/03-classes.ts
@@ -1,4 +1,4 @@
-
+import axios from 'axios';
 
 export class Pokemon {
 
@@ -10,20 +10,29 @@ export class Pokemon {
         public readonly id: number,
         public name: string,
         // public imageUrl: string
-    ) {}
+    ) { }
 
-    scream(){
+    scream() {
         console.log(`${this.name.toLocaleUpperCase()}!!!`)
     }
-    speak(){
+    speak() {
         console.log(`${this.name},${this.name}`)
     }
-        
+    async getMoves() {
+        const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/25`)
+        console.log(data.moves)
+
+        return data.moves
+
+    }
+
 }
 
 
 export const pikachu = new Pokemon(25, 'pikachu');
 
-console.log(pikachu);
-pikachu.scream();
-pikachu.speak();
+// console.log(pikachu);
+// pikachu.scream();
+// pikachu.speak();
+// console.log(pikachu.getMoves());
+pikachu.getMoves()
