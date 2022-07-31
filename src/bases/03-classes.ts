1937998e2d0dd0ebaf8caf78ccaa726a85d4d74c
PrintHolamundo <jdohca@gmail.com>
2022-07-31 18:49:51 -0500

    Colocar tipo de dato a respuestas http (genéricos)

--- src/bases/03-classes.ts
+++ src/bases/03-classes.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { Move, PokeapiResponse } from '../interfaces/pokeapi-response.interface';
 
 export class Pokemon {
 
@@ -18,8 +19,8 @@ export class Pokemon {
     speak() {
         console.log(`${this.name},${this.name}`)
     }
-    async getMoves() {
-        const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/25`)
+    async getMoves(): Promise<Move[]>{
+        const { data } = await axios.get<PokeapiResponse>(`https://pokeapi.co/api/v2/pokemon/25`)
         console.log(data.moves)
 
         return data.moves
