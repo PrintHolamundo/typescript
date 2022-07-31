1472a4d1b076e70f49b7718da2e02c8c24399737
PrintHolamundo <jdohca@gmail.com>
2022-07-31 15:59:34 -0500

    Getters , metodos y THIS

--- src/bases/03-classes.ts
+++ src/bases/03-classes.ts
@@ -2,13 +2,28 @@
 
 export class Pokemon {
 
+    get imageUrl(): string {
+        return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${this.id}.png`
+    }
+
     constructor(
         public readonly id: number,
-        public name: string) {
-        this.id = id;
-        this.name = name;
+        public name: string,
+        // public imageUrl: string
+    ) {}
+
+    scream(){
+        console.log(`${this.name.toLocaleUpperCase()}!!!`)
     }
+    speak(){
+        console.log(`${this.name},${this.name}`)
+    }
+        
 }
 
 
-export const pikachu = new Pokemon(4, 'pikachu');
+export const pikachu = new Pokemon(25, 'pikachu');
+
+console.log(pikachu);
+pikachu.scream();
+pikachu.speak();
